348edd8d592240dc0b8cf38205feec8dff4ac27a
stvort <stvort@mail.ru>
2019-06-13 22:24:18 +0400

    config

--- src/main/java/ru/otus/example/advancedconfigurationdemo/AdvancedConfigurationDemoApplication.java
+++ src/main/java/ru/otus/example/advancedconfigurationdemo/AdvancedConfigurationDemoApplication.java
@@ -2,7 +2,6 @@ package ru.otus.example.advancedconfigurationdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class AdvancedConfigurationDemoApplication {
